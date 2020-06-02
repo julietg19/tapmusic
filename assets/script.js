99a768fe94e9688b8fc5422a725dbe04ab1d65c9
Juliet Goldstein <thealamogirlsings@gmail.com>
2020-06-01 19:25:30 -0500

    files_UI_Styling

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", () => {
+  const sounds = document.querySelectorAll(".sound");
+
+  const pads = document.querySelectorAll(".pads div");
+
+  // sound
+
+  pads.forEach((pad) => {
+    pad.addEventListener("click", function () {});
+  });
+});
+
+// research arrow functions l
