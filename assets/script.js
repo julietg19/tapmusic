705837a0e463f55ebe2e3391cad66f21822031f8
Juliet Goldstein <thealamogirlsings@gmail.com>
2020-06-01 22:51:12 -0500

    soundnvisuals

--- assets/script.js
+++ assets/script.js
@@ -3,11 +3,38 @@ window.addEventListener("load", () => {
 
   const pads = document.querySelectorAll(".pads div");
 
+  const visual = document.querySelector(".visual");
+
+  const colors = [
+    "#ffadad",
+    "#ffd6a5",
+    "#fdffb6",
+    "#caffbf",
+    "#9bf6ff",
+    "#a0c4ff",
+  ];
+
   // sound
 
-  pads.forEach((pad) => {
-    pad.addEventListener("click", function () {});
-  });
-});
+  pads.forEach((pad, index) => {
+    pad.addEventListener("click", function () {
+      sounds[index].currentTime = 0;
 
-// research arrow functions l
+      sounds[index].play();
+
+      createBubbles(index);
+    });
+  });
+
+  //create visual function
+
+  const createBubbles = (index) => {
+    const bubble = document.createElement("div");
+    visual.appendChild(bubble);
+    bubble.style.backgroundColor = colors[index];
+    bubble.style.animation = "jump 1s ease";
+    bubble.addEventListener("animationend", function () {
+      visual.removeChild(this);
+    });
+  };
+});
